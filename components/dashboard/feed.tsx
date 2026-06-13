f1228ebbad7527a4ff0690e3aea92b38ba161719
linus145 <lakklplbros@gmail.com>
2026-06-13 08:54:25 +0530

    Ai chnages

--- components/dashboard/feed.tsx
+++ components/dashboard/feed.tsx
@@ -124,7 +124,7 @@ export function Feed({ isCollapsed, isRightCollapsed, onNavigateToProfile }: Fee
         </div>
       </div> */}
 
-      <div className="max-w-3xl mx-auto space-y-4 transition-transform">
+      <div className="max-w-[52rem] mx-auto space-y-4 transition-transform">
 
         {/* Composer (Open Modal Trigger) */}
         <div
